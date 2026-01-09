4f3e813449e8c276abd31a042cc0857592ae338b
Ruben <42184865+Morulo90@users.noreply.github.com>
2026-01-09 12:20:10 +0100

    Add marketplace contracts and tests

--- test/Marketplace.test.ts
+++ test/Marketplace.test.ts
@@ -1,5 +1,5 @@
-import { expect } from "chai";
-import { ethers } from "hardhat";
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 describe("NFT Marketplace", function () {
     it("lists and buys with fee + royalty", async function () {
